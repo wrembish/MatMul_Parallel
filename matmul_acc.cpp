033509d4d597ce935227f7641ca425cd245a02db
wrembish <wrembish@udel.edu>
2019-12-02 19:23:21 -0500

    more openacc

--- matmul_acc.cpp
+++ matmul_acc.cpp
@@ -37,9 +37,6 @@ int main()
 		for(int unsigned j = 0; j < MAT2_Y; j++)
 		{
 			result_mat[i][j] = 0;
-			cout << "success" << endl;
-			result_mat_new[i][j] = 0;
-			cout << "success" << endl;
 		}
 	}
 	// fill in mat1 with random positive integers <= 100
