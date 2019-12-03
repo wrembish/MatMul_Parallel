e9a75cace14132ec22ee514d374791a535092613
wrembish <wrembish@udel.edu>
2019-12-02 20:21:02 -0500

    more openacc

--- matmul_acc.cpp
+++ matmul_acc.cpp
@@ -62,8 +62,8 @@ int main()
 	// if the matrices can be multiplied, do it
 	if(MAT1_Y == MAT2_X)
 	{
-		//#pragma omp parallel for ordered schedule(auto) collapse(3)
-		#pragma acc data copyout(result_mat[0:MAT1_X-1][0:MAT2_Y-1]), copyin(mat1[0:MAT1_X-1][0:MAT1_Y-1],mat2[0:MAT2_X-1][0:MAT2_Y-1])
+		#pragma omp parallel for ordered schedule(auto) collapse(3)
+		//#pragma acc data copyout(result_mat[0:MAT1_X-1][0:MAT2_Y-1]), copyin(mat1[0:MAT1_X-1][0:MAT1_Y-1],mat2[0:MAT2_X-1][0:MAT2_Y-1])
 		//#pragma kernals
 		for(int unsigned i = 0; i < MAT1_X; i++)
 		{
